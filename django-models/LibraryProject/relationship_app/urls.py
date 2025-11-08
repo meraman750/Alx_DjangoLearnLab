1a67b7d51d59d0249a91d867ee1347c75c4e8045
Meheretabe Abayneh <meraman675@gmail.com>
2025-11-08 06:06:34 +0300

    advanced relationships

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -8,16 +8,17 @@ urlpatterns = [
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
+    path('books/add/', add_book, name='add_book'),          # <-- add_book/
+    path('books/<int:pk>/edit/', edit_book, name='edit_book'),  # <-- edit_book/
+    path('books/<int:pk>/delete/', delete_book, name='delete_book'),
+
     # Authentication URLs
     path('register/', views.register, name='register'),  # Must be exactly views.register
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 
+
     path('admin-dashboard/', admin_view, name='admin_view'),
     path('librarian-dashboard/', librarian_view, name='librarian_view'),
     path('member-dashboard/', member_view, name='member_view'),
-
-    path('books/add/', add_book, name='add_book'),
-    path('books/<int:pk>/edit/', edit_book, name='edit_book'),
-    path('books/<int:pk>/delete/', delete_book, name='delete_book'),
 ]
