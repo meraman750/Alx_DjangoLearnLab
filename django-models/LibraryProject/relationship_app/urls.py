3f7e239def43c7842e93772249a0c3dedbc04018
Meheretabe Abayneh <meraman675@gmail.com>
2025-11-08 05:54:37 +0300

    advanced relationships

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
-from .views import LibraryDetailView, list_books
+from .views import LibraryDetailView, list_books, admin_view, member_view, librarian_view
 
 urlpatterns = [
     # Books and library
@@ -12,4 +12,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),  # Must be exactly views.register
     path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+    path('admin-dashboard/', admin_view, name='admin_view'),
+    path('librarian-dashboard/', librarian_view, name='librarian_view'),
+    path('member-dashboard/', member_view, name='member_view'),
 ]
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,6 +4,8 @@ from .models import Library, Book
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import user_passes_test
+
 
 
 # Create your views here.
@@ -27,3 +29,25 @@ class LibraryDetailView(DetailView):
     model = Library
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
+
+def is_admin(user):
+    return user.userprofile.role == "Admin"
+
+def is_librarian(user):
+    return user.userprofile.role == "Librarian"
+
+def is_member(user):
+    return user.userprofile.role == "Member"
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
+
