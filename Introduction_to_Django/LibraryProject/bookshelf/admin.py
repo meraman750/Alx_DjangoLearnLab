05ad44881760c0f7d2391f69a5a3851c3ff140fc
Meheretabe Abayneh <meraman675@gmail.com>
2025-11-08 04:41:55 +0300

    advanced relationships

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Book
 
 class BookAdmin(admin.ModelAdmin):
-  list_display = ('id', 'title', 'author', 'pulication_year')
+  list_display = ('id', 'title', 'author', 'publication_year')
   search_fields = ["title", "author"]
   list_filter = ["publication_year"]
 
