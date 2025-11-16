b135c8ad2ab1c4500ac0f1fbc591431b55a8ffa3
Meheretabe Abayneh <meraman675@gmail.com>
2025-11-16 15:56:28 +0300

    Custom user and custom manager update

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -14,21 +14,26 @@ class CustomUser(AbstractUser):
     def __str__(self):
         return self.username
     
-class CustomManager(BaseUserManager):
+class CustomUserManager(BaseUserManager):
+    """Custom manager for CustomUser"""
+
     def create_user(self, username, email, password=None, **extra_fields):
         if not email:
             raise ValueError("Users must have an email address")
-
         email = self.normalize_email(email)
         user = self.model(username=username, email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
-    
+
     def create_superuser(self, username, email, password=None, **extra_fields):
         extra_fields.setdefault("is_staff", True)
         extra_fields.setdefault("is_superuser", True)
         extra_fields.setdefault("is_active", True)
 
-        return self.create_user(username, email, password, **extra_fields)
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True")
 
+        return self.create_user(username, email, password, **extra_fields)
