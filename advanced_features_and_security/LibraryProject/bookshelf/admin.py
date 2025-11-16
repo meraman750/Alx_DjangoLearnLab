4c73e4b56a419bbd3c58d034b8aa643546004974
Meheretabe Abayneh <meraman675@gmail.com>
2025-11-16 15:57:54 +0300

    Custom user and custom manager update

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Book
+from django.contrib.auth.admin import UserAdmin
+from .models import Book, CustomUser
 
 class BookAdmin(admin.ModelAdmin):
   list_display = ('id', 'title', 'author', 'publication_year')
@@ -7,3 +8,29 @@ class BookAdmin(admin.ModelAdmin):
   list_filter = ["publication_year"]
 
 admin.site.register(Book, BookAdmin)
+
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+
+    list_display = ("username", "email", "date_of_birth", "is_staff", "is_active")
+    list_filter = ("is_staff", "is_active", "date_of_birth")
+
+    fieldsets = (
+        (None, {"fields": ("username", "email", "password")}),
+        ("Personal Information", {"fields": ("date_of_birth", "profile_photo")}),
+        ("Permissions", {"fields": ("is_staff", "is_active", "is_superuser", "groups", "user_permissions")}),
+    )
+
+    # Fields shown when creating a user
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "date_of_birth", "profile_photo", "password1", "password2", "is_staff", "is_active"),
+        }),
+    )
+
+    search_fields = ("email", "username")
+    ordering = ("username",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
