803cea99e65c6dc8c8e29053bfcab8d22748d5dd
Meheretabe Abayneh <meraman675@gmail.com>
2025-11-23 02:35:49 +0300

    Basic configuration

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from .models import Book
+from .serializers import BookSerializer
+from rest_framework import generics
 # Create your views here.
+
+
+class BookList(generics.ListAPIView):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
