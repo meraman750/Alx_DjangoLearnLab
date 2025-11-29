6e9c4eff5f1e8f025e0e22a0be3d36eb48cebc7c
Meheretabe Abayneh <meraman675@gmail.com>
2025-11-29 22:12:05 +0300

    Advanced serializers api

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,7 +1,9 @@
-from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, generics, permissions, filters
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend 
 from .models import Book
 from .serializers import BookSerializer
+from django_filters import rest_framework
+from rest_framework import generics
 
 # ----------------------------
 # ListView - Retrieve all books
