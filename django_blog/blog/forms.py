401c1bfa2c2bb14894ae05f4eb1f427f6a783196
Meheretabe Abayneh <meraman675@gmail.com>
2025-12-07 21:08:26 +0300

    Basic authentication.

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -2,6 +2,8 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile, Post, Comment, Tag
+from taggit_labels.widgets import TagWidget
+
 
 class UserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -36,6 +38,10 @@ class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content", "tags"]
+
+        widgets = {
+            "tags": TagWidget()
+        }
 
     def save(self, commit=True):
         post = super().save(commit=False)
