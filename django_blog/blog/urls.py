37b7cd8659a418a44c6d9046ea616d9b16619870
Meheretabe Abayneh <meraman675@gmail.com>
2025-12-07 17:25:04 +0300

    Basic authentication.

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -9,10 +9,10 @@ urlpatterns = [
     path("profile/", views.profile, name="profile"),
     path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(), name="logout"),
-    path("posts/", views.PostListView.as_view(), name="post-list"),
-    path("posts/new/", views.PostCreateView.as_view(), name="post-create"),
-    path("posts/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
-    path("posts/<int:pk>/edit/", views.PostUpdateView.as_view(), name="post-update"),
-    path("posts/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete"),
+    path("post/", views.PostListView.as_view(), name="post-list"),
+    path("post/new/", views.PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
+    path("post/<int:pk>/update/", views.PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete"),
 
 ]
