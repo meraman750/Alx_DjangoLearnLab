720549df0db93a91dfb07ff42548a1e0b49d1189
Meheretabe Abayneh <meraman675@gmail.com>
2025-12-14 20:42:20 +0300

    Basic authentication

---
+++ social_media_api/posts/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from .views import PostViewSet, CommentViewSet
+
+router = DefaultRouter()
+router.register(r"posts", PostViewSet, basename="posts")
+router.register(r"comments", CommentViewSet, basename="comments")
+
+urlpatterns = [
+    path("", include(router.urls)),
+]
