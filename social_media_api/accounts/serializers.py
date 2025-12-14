762ae955423bc720b907f48b342a19aab8efb89c
Meheretabe Abayneh <meraman675@gmail.com>
2025-12-14 20:20:41 +0300

    Basic authentication

--- social_media_api/accounts/serializers.py
+++ social_media_api/accounts/serializers.py
@@ -1,20 +1,23 @@
 from rest_framework import serializers
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate, get_user_model
 from rest_framework.authtoken.models import Token
 from .models import User
+
+User = get_user_model()  
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ["username", "email", "password", "bio"]
+        fields = ("username", "email", "password", "bio")
 
     def create(self, validated_data):
+        # Use get_user_model().objects.create_user
         user = User.objects.create_user(
             username=validated_data["username"],
-            email = validated_data.get("email"), 
-            password=validated_data["password"], 
+            email=validated_data.get("email"),
+            password=validated_data["password"],
             bio=validated_data.get("bio", "")
         )
         Token.objects.create(user=user)
